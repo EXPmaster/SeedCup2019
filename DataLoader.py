4e051e89c0e7856d64a0224547a53bcfc05b96ee
Riser6 <1528152501@qq.com>
2019-09-27 23:15:09 +0800

    commit 9.27

---
+++ DataLoader.py
@@ -0,0 +1,139 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import re
+
+train_file = './SeedCup2019_pre/SeedCup_pre_train.csv'
+test_file = './SeedCup2019_pre/SeedCup_pre_test.csv'
+pd.set_option('display.max_columns', None)
+
+
+def fix_time(date_str):
+    # 修复时间无效值
+    a = []
+    # df = pd.DataFrame()
+    pi = r'\d+-\d+-\d+ \d+:\d+:\d+'
+    pa = re.compile(pi)
+    print('replacing...')
+    for i in range(len(dataset[date_str])):
+        key = dataset[date_str][i]
+        # print(train_reader['got_time'][i])
+        # print(pa.findall(key))
+        if pa.findall(key) == a:
+            # print(train_reader['dlved_time'][i],i)
+            if date_str == 'dlved_time':
+                dataset[date_str][i] = dataset[date_str][i].replace(
+                    '-99', '2019-03-03 11:20:30')
+            else:
+                dataset[date_str][i] = dataset[date_str][i].replace(
+                    '-99', '2019-03-02 17:37:05')
+    print('replace ' + date_str + ' finished')
+
+
+def showfreq(time):
+    d_freq = {}
+    for date in time:
+        if date not in d_freq:
+            d_freq[date] = 1
+        else:
+            d_freq[date] += 1
+    print(sorted(d_freq.items(), key=lambda item: item[1]))
+
+def getfreq(time):
+    d_freq = {}
+    for date in time:
+        if date not in d_freq:
+            d_freq[date] = 1
+        else:
+            d_freq[date] += 1
+    return dict(sorted(d_freq.items(), key=lambda item: item[1], reverse=True))
+
+
+def handle_uid(uid):
+    usr_id_idx = []
+    usr_freq = getfreq(uid)
+    for item in uid.values:
+        usr_id_idx.append(usr_freq[item])
+    return usr_id_idx
+
+def handle_switch(first_time, second_time):
+    if ((first_time - second_time).days * 24 +
+            (first_time - second_time).seconds // 3600) < 0:
+        time =  (second_time - first_time).days * 24 + (second_time -
+                                                       first_time).seconds // 3600
+    else:
+        time =  (first_time - second_time).days * 24 + (first_time -
+                                                       second_time).seconds // 3600
+    if time > 400:
+        time = 400
+    return time
+
+def get_time_diff():
+    print('getting time diff...')
+    pay_ship_diff = []
+    ship_get_diff = []
+    get_dlv_diff = []
+    dlv_sign_diff = []
+    day_in_total = []
+    hour_in_signed=[]
+    for i in range(len(dataset['got_time'])):
+        pay_ship_diff.append(handle_switch(shipped_time[i], payed_time[i]))
+        ship_get_diff.append(handle_switch(shipped_time[i], got_time[i]))
+        get_dlv_diff.append(handle_switch(dlved_time[i], got_time[i]))
+        dlv_sign_diff.append(handle_switch(signed_time[i], dlved_time[i]))
+        day_in_total.append(min(20, abs((signed_time[i]-payed_time[i]).days)))
+        hour_in_signed.append(max(7, signed_time[i].hour))
+
+    times = {
+        'pay_ship': pay_ship_diff,
+        'ship_get': ship_get_diff,
+        'get_dlv': get_dlv_diff,
+        'dlv_sign': dlv_sign_diff,
+        'total_day': day_in_total,
+        'sign_hour':hour_in_signed
+    }
+    time_diff_data = pd.DataFrame(times)
+    print('finished')
+    #print(time_diff_data)
+    return time_diff_data
+
+
+if __name__ == '__main__':
+    print('Loading files...')
+    dataset = pd.read_csv(train_file, sep='\t')
+    #dataset=dataset[:1000]
+    # 修复时间的错误值
+    fix_time('got_time')
+    fix_time('dlved_time')
+    # 发货时间
+    shipped_time = pd.to_datetime(dataset['shipped_time'])
+    # 揽件时间
+    got_time = pd.to_datetime(dataset['got_time'])
+    # 走件时间
+    dlved_time = pd.to_datetime(dataset['dlved_time'])
+    # 订单支付时间
+    payed_time = pd.to_datetime(dataset['payed_time'])
+    # 签收时间
+    signed_time = pd.to_datetime(dataset['signed_time'])
+    # 时间差
+    time_diff = get_time_diff()
+
+    # TODO 商家公司频率
+    frames = [dataset, time_diff]
+    new_dataset = pd.concat(frames, axis=1)
+    del dataset
+    # target = np.array(new_dataset['total_time'])
+    # train_data = np.array(new_dataset.drop(['total_time'], axis=1))
+    print('load data finished')
+    print('saving dataset...')
+    new_dataset.to_csv('train_data.csv')
+    print('data saved successfully')
+    # 可视化
+    """
+    for item in dataset.keys():
+        if not re.match('[a-z]*_time', item) and item != 'preselling_shipped_time':
+            sns.distplot(dataset[item])
+            print(item + ' finished')
+            plt.show()
+    """
--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 import pandas as pd
 import datetime
 from sklearn.model_selection import train_test_split
+from DataLoader import handle_uid
+from evaluation import calculateAllMetrics
+from sklearn.datasets import dump_svmlight_file
 from sklearn import preprocessing
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -11,48 +14,42 @@ import re
 pd.set_option('display.max_columns', None)
 
 data_file = './train_data.csv'
-test_file = './SeedCup2019_pre/SeedCup_pre_test.csv'
+test_file = 'SeedCup_pre_test.csv'
 submit_file = './submit.txt'
-read_rows = 150000
+read_rows = 12
 
 
-def time_predict(pay_time, total):
+# TODO 地理特征处理
+def time_predict(pay_time, days, hours):
     # pay_time为支付时间，total为小时数
     i = 0
     date = []
+    print(days, hours)
     for item in pay_time:
-        delta = datetime.timedelta(days=total[i] // 24, hours=total[i] % 24)
+        delta = datetime.timedelta(days=int(days[i]), hours=int(hours[i] + 7))
         new_date = item + delta
+        if new_date.hour <= 6:
+            new_date += datetime.timedelta(days=0, hours=10)
         date.append(new_date)
         i += 1
     return date  # 返回新世间
 
 
-def onTimePercent(pred_date, real_date):
-    real_date = list(real_date)
-    total = len(pred_date)
-    count = 0
-    for i in range(total):
-        if (pred_date[i] - real_date[i]).days <= 0:
-            count += 1
-    print('on time percent: %lf' % (count / total))
-
-
-def rankScore(real_signed_time, pred_signed_time):
-    loss = []
-    i = 0
-    for item in real_signed_time:
-        if (pred_signed_time[i] - item).days * 24 + \
-                (pred_signed_time[i] - item).seconds // 3600 > 0:
-            loss.append((pred_signed_time[i] - item).days * 24 +
-                        (pred_signed_time[i] - item).seconds // 3600)
+def fill_blank(dataset, validset, col_from, col_to):
+    print('filling {} blanks...'.format(col_to))
+    mosts = dataset[col_to].value_counts().index[0]
+    array = []
+    for item in validset[col_from]:
+        try:
+            idx = dataset[dataset[col_from] == item].index.tolist()[0]
+        except IndexError:
+            array.append(mosts)
         else:
-            loss.append((item - pred_signed_time[i]).days * 24 +
-                        (item - pred_signed_time[i]).seconds // 3600)
-        i += 1
-    import math
-    mse = math.sqrt(sum([val ** 2 for val in loss]) / len(loss))
-    print('MSE: %lf' % mse)
+            array.append(dataset[col_to].loc[idx])
+    print('finished')
+    validset[col_to].loc[:] = array[:]
+    # print(validset[col_to])
+    return validset[col_to]
 
 
 def gen_dataset(dataset, validset, mode='valid'):
@@ -64,14 +61,17 @@ def gen_dataset(dataset, validset, mode='valid'):
     # 验证集收货时间
     if mode == 'valid':
         valid_signed_time = pd.to_datetime(validset['signed_time'])
-
     len_train = len(dataset)
     # 填补特征
+
     feature = ['lgst_company', 'warehouse_id', 'shipped_prov_id',
-    'shipped_city_id', 'pay_ship', 'ship_get',
-    'get_dlv', 'dlv_sign']
+               'shipped_city_id']
     for item in feature:
         validset[item] = np.nan
+
+    # validset['shipped_prov_id'] = fill_blank(dataset, validset, 'seller_uid', 'shipped_prov_id')
+    # validset['shipped_prov_id'] = fill_blank(dataset, validset, 'rvcr_prov_name', 'shipped_prov_id')
+    # validset['shipped_city_id'] = fill_blank(dataset, validset, 'seller_uid', 'shipped_city_id')
     # 构造长据集
     dataset = pd.concat([dataset, validset], ignore_index=True)
 
@@ -79,12 +79,22 @@ def gen_dataset(dataset, validset, mode='valid'):
     for item in dataset.keys():
         dataset[item] = dataset[item].fillna(dataset[item].value_counts().index[0])
         # dataset[item] = dataset[item].fillna(-9999)
-
-    # print(dataset)
-    # 处理id, 归一化[0, 1]
+    # 归一化时间
     """
-    product_id = (dataset['product_id'] - dataset['product_id'].min()) /\
-                 (dataset['product_id'].max() - dataset['product_id'].min())
+    pay_ship_norm = (dataset['pay_ship'] - dataset['pay_ship'].min()) /\
+                 (dataset['pay_ship'].max() - dataset['pay_ship'].min())
+    ship_get_norm = (dataset['ship_get'] - dataset['ship_get'].min()) /\
+                 (dataset['ship_get'].max() - dataset['ship_get'].min())
+    get_dlv_norm = (dataset['get_dlv'] - dataset['get_dlv'].min()) /\
+                 (dataset['get_dlv'].max() - dataset['get_dlv'].min())
+    dlv_sign_norm = (dataset['dlv_sign'] - dataset['dlv_sign'].min()) /\
+                 (dataset['dlv_sign'].max() - dataset['dlv_sign'].min())
+    """
+    # print(dataset)
+    # 处理id
+    product_id = handle_uid(dataset['product_id'])
+    product_id_dummy = pd.get_dummies(product_id, prefix=product_id)
+    """
 
     seller_uid = (dataset['seller_uid'] - dataset['seller_uid'].min()) /\
                  (dataset['seller_uid'].max() - dataset['seller_uid'].min())
@@ -108,10 +118,10 @@ def gen_dataset(dataset, validset, mode='valid'):
     # 三级类目
     cate3_id_dummy = pd.get_dummies(
         dataset['cate3_id'], prefix=dataset[['cate3_id']].columns[0])
-    # TODO 商家公司id, 商品id, pre_selling, 买家uid 未使用
+    # TODO 商家公司id, pre_selling, 买家uid 未使用
     # 商家id
-    # seller_uid_dummy = pd.get_dummies(
-        # dataset['seller_uid'], prefix=dataset[['seller_uid']].columns[0])
+    seller_uid_dummy = pd.get_dummies(
+        dataset['seller_uid'], prefix=dataset[['seller_uid']].columns[0])
     # 物流公司id
     lgst_company_dummy = pd.get_dummies(
         dataset['lgst_company'], prefix=dataset[['lgst_company']].columns[0])
@@ -130,8 +140,8 @@ def gen_dataset(dataset, validset, mode='valid'):
     # 收货城市id
     rvcr_city_name_dummy = pd.get_dummies(
         dataset['rvcr_city_name'], prefix=dataset[['rvcr_city_name']].columns[0])
-    frames = [plat_form_dummy, biz_type_dummy, cate1_id_dummy, cate2_id_dummy,
-              cate3_id_dummy, lgst_company_dummy, warehouse_id_dummy,
+    frames = [plat_form_dummy, biz_type_dummy, product_id_dummy, cate1_id_dummy, cate2_id_dummy,
+              cate3_id_dummy, seller_uid_dummy, lgst_company_dummy, warehouse_id_dummy,
               shipped_prov_id_dummy, shipped_city_id_dummy, rvcr_prov_name_dummy,
               rvcr_city_name_dummy]
     new_dataset = pd.concat(frames, axis=1)
@@ -139,15 +149,16 @@ def gen_dataset(dataset, validset, mode='valid'):
     train_set = new_dataset[:len_train]
     valid_set = new_dataset[len_train:]
     if mode == 'valid':
-        return train_set, valid_set, train_begin_time,\
+        return train_set, valid_set, train_begin_time, \
                valid_begin_time, train_signed_time, valid_signed_time
     else:
         return train_set, valid_set, train_begin_time, valid_begin_time, train_signed_time
 
 
-def load_data(mode='valid', label='total_time'):
+def load_data(mode='valid', label1='total_day',label2='sign_hour'):
     print('loading dataset...')
     train_data = pd.read_csv('train_data.csv', nrows=read_rows)
+
     # 可视化
     """
     for item in train_data.keys():
@@ -155,16 +166,32 @@ def load_data(mode='valid', label='total_time'):
             sns.distplot(train_data[item])
             print(item + ' finished')
             plt.show()
-            
+
     sns.distplot(train_data['product_id'])
     plt.show()
     sns.distplot(train_data['seller_uid'])
     plt.show()
-    # sns.regplot(x='product_id', y=label, data=train_data)
-    # plt.show()
+
+    sns.regplot(x='seller_uid', y='shipped_city_id', data=train_data)
+    plt.show()
+    sns.regplot(x='seller_uid', y='shipped_prov_id', data=train_data)
+    plt.show()
+    sns.regplot(x='total_time', y='shipped_city_id', data=train_data)
+    plt.show()
+    sns.regplot(x='total_time', y='shipped_prov_id', data=train_data)
+    plt.show()
+    sns.regplot(x='rvcr_prov_name', y='total_time', data=train_data)
+    plt.show()
     """
-    target = train_data[label]
-    train_data = train_data.drop([label], axis=1)
+    target1 = train_data[label1]
+    '''a=[0]*21
+    for i in target1:
+        a[i]+=1
+    print(a)'''
+    target2 = train_data[label2]
+    target = np.array((target1, target2)).transpose()
+    train_data = train_data.drop([label1, label2], axis=1)
+    #print(len(train_data))
     if mode == 'valid':
         train_set, valid_set, train_target, valid_target = train_test_split(
             train_data, target, test_size=0.5)
@@ -172,46 +199,122 @@ def load_data(mode='valid', label='total_time'):
         train_set, valid_set, train_begin_time, valid_begin_time, \
         train_signed_time, valid_signed_time = gen_dataset(train_set, valid_set, mode='valid')
         print('load finished')
-        return np.array(train_set), np.array(valid_set), np.array(train_target), \
-               np.array(valid_target), train_begin_time, \
+        return np.array(train_set), np.array(valid_set), train_target, \
+               valid_target, train_begin_time, \
                train_signed_time, valid_begin_time, valid_signed_time
 
     else:
+        # train_data.sample(frac=0.66, replace=True, random_state=None)
         valid_set = pd.read_csv(test_file, sep='\t')
         train_set, valid_set, train_begin_time, valid_begin_time, \
         train_signed_time = gen_dataset(train_data, valid_set, mode='test')
         print('load finished')
-        return np.array(train_set), np.array(valid_set), np.array(target), \
+        return np.array(train_set), np.array(valid_set), target, \
                train_begin_time, train_signed_time, valid_begin_time
 
 
-def train():
-    print('training...')
+def my_loss_fun(y_pred, y_true):
+    n = len(y_pred)
+    y_true = y_true.get_label()
+
+    # loss = ((y_pred - y_true) ** 2).mean()
+    grad = 2 * (y_pred - y_true) / n
+    hess = np.ones((n, 1))
+    hess *= 2 / n
     """
-    param = {'max_depth':4, 'eta': 0.1}
-    num_round = 9
+    if np.sum(y_pred) > np.sum(y_true):
+        loss = np.sqrt(((y_pred - y_true) ** 2).mean()) * 10
+        grad = 10 * (y_pred - y_true) * (n * np.sum((y_pred - y_true) ** 2)) ** (-0.5)
+        hess = 10 * y_pred * (n * np.sum(((y_pred - y_true) ** 2))) ** (-0.5) - n ** (-0.5) *\
+               (np.sum(((y_pred - y_true) ** 2))) ** (-1.5) * (y_pred - y_true) ** 2
+    else:
+        loss = np.sqrt(((y_pred - y_true) ** 2).mean())
+        grad = (y_pred - y_true) * (n * np.sum((y_pred - y_true) ** 2)) ** (-0.5)
+        hess = y_pred * (n * np.sum(((y_pred - y_true) ** 2))) ** (-0.5) - n ** (-0.5) * \
+               (np.sum(((y_pred - y_true) ** 2))) ** (-1.5) * (y_pred - y_true) ** 2
+               """
+    return grad, hess
+
+
+def train(train_set, train_target, valid_set, valid_target=None):
+    print('training...')
+    train_target1 = train_target[:, 0]
+
+    train_target2 = train_target[:, 1] - 7
+    # print(train_target2)
+    valid_target1 = valid_target[:, 0]
+    valid_target2 = valid_target[:, 1] - 7
+    dtrain = xgb.DMatrix(train_set, label=train_target1)
+    del train_target1
+    """
+    param = {'max_depth': 4, 'eta': 0.1}
+    num_round = 5
     bst = xgb.train(param, dtrain, num_round)
     """
-    params = {
+    param1 = {
         'booster': 'gbtree',
-        # 'colsample_bytree': 0.7,
+        'colsample_bytree': 0.8,
         'eta': 0.1,
         'max_depth': 4,
         'objective': 'reg:squarederror',
-        # 'subsample': 0.5,
-        # 'min_child_weight': 3
+        'gamma': 0.2,
+        # 'subsample': 1.0,
+        'min_child_weight': 5
+
         # 'tree_method': 'gpu_hist'
     }
-    num_round = 9
-    bst = xgb.train(params, dtrain, num_round)
+    param2 = {
+        'booster': 'gbtree',
+        'colsample_bytree': 0.8,
+        'eta': 0.103,
+        'num_class': 17,
+        'max_depth': 40,
+        'objective': 'multi:softmax',
+        'gamma': 0.2,
+        # 'subsample': 1.0,
+        'min_child_weight': 5,
+        'eval_metric': 'merror'
 
-    print('train finished')
+        # 'tree_method': 'gpu_hist'
+    }
+    param3 = {
+        'booster': 'gbtree',
+        'colsample_bytree': 0.8,
+        'eta': 0.5,
+        'num_class': 21,
+        'max_depth': 15,
+        'objective': 'multi:softmax',
+        'gamma': 0.2,
+        # 'subsample': 1.0,
+        'min_child_weight': 20,
+        'eval_metric': 'merror'
+
+        # 'tree_method': 'gpu_hist'
+    }
+    num_round1 = 20
+    num_round2 = 20
+
+    bst = xgb.train(param3, dtrain, num_round1)
+
+    del dtrain
+    dvalid = xgb.DMatrix(valid_set, label=valid_target1)
+    # dvalid = xgb.DMatrix(valid_set)
     # make prediction
-    predict_total_hours = bst.predict(dvalid)
-    predict_date = time_predict(valid_begin_time, predict_total_hours)
+    predict_total_days = bst.predict(dvalid)
+    # predict_date = time_predict(valid_begin_time, predict_total_hours)
+    del dvalid
 
-    onTimePercent(predict_date, valid_signed_time)
-    rankScore(valid_signed_time, predict_date)
+    dtrain = xgb.DMatrix(train_set, label=train_target2)
+
+    bst2 = xgb.train(param2, dtrain, num_round2)
+    del dtrain
+    dvalid = xgb.DMatrix(valid_set, label=valid_target2)
+    predict_total_hours = bst2.predict(dvalid)
+    del dvalid
+    predict_date = time_predict(valid_begin_time, predict_total_days, predict_total_hours)
+    print('train finished')
+    otp, rs = calculateAllMetrics(valid_signed_time, predict_date)
+    print('on time percent: %lf\nrank score: %lf' % (otp, rs))
 
     return predict_date
 
@@ -220,8 +323,8 @@ def submit(date):
     print('writing to file...')
     with open(submit_file, 'w') as f:
         for i in range(len(date)):
-            frmt = str(date[i].year) + '-' + str(date[i].month) +\
-                  '-' + str(date[i].day)
+            frmt = str(date[i].year) + '-' + str(date[i].month) + \
+                   '-' + str(date[i].day)
             f.write(frmt + ' ' + str(date[i].hour) + '\n')
     print('finished')
 
@@ -231,15 +334,14 @@ if __name__ == '__main__':
 
     train_set, valid_set, train_target, valid_target, train_begin_time, \
     train_signed_time, valid_begin_time, valid_signed_time = load_data(mode='valid')
-    
+
     """
     train_set, valid_set, train_target, train_begin_time, \
     train_signed_time, valid_begin_time = load_data(mode='test')
     """
-    
-    dtrain = xgb.DMatrix(train_set, label=train_target)
-    dvalid = xgb.DMatrix(valid_set, label=valid_target)
-    # dvalid = xgb.DMatrix(valid_set)
-    date = train()
+
+    date = train(train_set, train_target, valid_set, valid_target)
+    # date = train(train_set, train_target, valid_set)
 
     # submit(date)
+
